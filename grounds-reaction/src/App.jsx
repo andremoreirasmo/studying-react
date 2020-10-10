1a7521750c08d5dd99b25742dff1777edb441d8c
André Luis Moreira <andreluismoreirasmo@gmail.com>
2020-10-10 20:53:12 -0300

    Created random component

--- grounds-reaction/src/App.jsx
+++ grounds-reaction/src/App.jsx
@@ -3,16 +3,19 @@ import React from 'react'
 import First from './components/basic/First'
 import WithParameter from './components/basic/WithParameter'
 import Fragment from './components/basic/Fragment'
+import Random from './components/basic/Random'
 
-export default _ => 
-    <div id="app"> 
+export default () => (
+    <div id="app">
         <h1>Fundamentos React</h1>
         <br></br>
+        <Random min={10} max ={60}/>
         <Fragment></Fragment>
-        <WithParameter 
+        <WithParameter
             title="Primeiro titulo"
             student="André Luis Moreira"
             score={7.2}
         />
         <First></First>
     </div>
+)
