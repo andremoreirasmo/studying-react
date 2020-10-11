e7b5c9edd89cdb985eed49861c0d36e349e4b7ff
André Luis Moreira <andreluismoreirasmo@gmail.com>
2020-10-10 21:15:19 -0300

    Added card component

--- grounds-reaction/src/App.jsx
+++ grounds-reaction/src/App.jsx
@@ -4,18 +4,31 @@ import First from './components/basic/First'
 import WithParameter from './components/basic/WithParameter'
 import Fragment from './components/basic/Fragment'
 import Random from './components/basic/Random'
+import Card from './components/layout/Card'
 
 export default () => (
     <div id="app">
         <h1>Fundamentos React</h1>
         <br></br>
-        <Random min={10} max ={60}/>
-        <Fragment></Fragment>
-        <WithParameter
-            title="Primeiro titulo"
-            student="André Luis Moreira"
-            score={7.2}
-        />
-        <First></First>
+
+        <Card title="#04 - Desafio Aleatório" >
+            <Random min={10} max={60} />
+        </Card>
+
+        <Card title="#03 - Fragmento">
+            <Fragment></Fragment>
+        </Card>
+
+        <Card title="#02 - Com parâmetro">
+            <WithParameter
+                title="Primeiro titulo"
+                student="André Luis Moreira"
+                score={7.2}
+            />
+        </Card>
+
+        <Card title="#01 - Primeiro componente">
+            <First></First>
+        </Card>
     </div>
 )
